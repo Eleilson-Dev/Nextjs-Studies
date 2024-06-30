dbd42ac8b2c5f415e767dfd6863902391fb3a441
Mxt@DEV <maxta.gamer@gmail.com>
2024-06-30 00:05:31 -0300

    update

--- 01-firstproject-nextjs/src/app/page.tsx
+++ 01-firstproject-nextjs/src/app/page.tsx
@@ -4,19 +4,21 @@ import heroImg from '../../public/assets/hero.png';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.logoContent}>
-        <Image
-          className={styles.hero}
-          alt="logo tarefas"
-          src={heroImg}
-          priority
-        />
-      </div>
-      <h1>
-        sitema feito pra vc organizar <br />
-        seus estudos e tarefas
-      </h1>
-    </main>
+    <div className={styles.conteiner}>
+      <main>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="logo tarefas"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1>
+          sitema feito pra vc organizar <br />
+          seus estudos e tarefas
+        </h1>
+      </main>
+    </div>
   );
 }

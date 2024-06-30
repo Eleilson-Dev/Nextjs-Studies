a13c1d5df35ff9560c5fa8b134d2b3f82f685584
Mxt@DEV <maxta.gamer@gmail.com>
2024-06-29 23:58:37 -0300

    update

---
+++ 01-firstproject-nextjs/src/app/page.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/home.module.scss';
+import Image from 'next/image';
+import heroImg from '../../public/assets/hero.png';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.logoContent}>
+        <Image
+          className={styles.hero}
+          alt="logo tarefas"
+          src={heroImg}
+          priority
+        />
+      </div>
+      <h1>
+        sitema feito pra vc organizar <br />
+        seus estudos e tarefas
+      </h1>
+    </main>
+  );
+}

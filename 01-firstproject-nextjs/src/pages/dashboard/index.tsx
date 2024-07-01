87dea76cc005b8f9a39c6a1f9b119a2680b8d018
Mxt@DEV <maxta.gamer@gmail.com>
2024-06-30 22:08:21 -0300

    update

--- 01-firstproject-nextjs/src/pages/dashboard/index.tsx
+++ 01-firstproject-nextjs/src/pages/dashboard/index.tsx
@@ -1,9 +1,34 @@
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 import styles from './styles.module.scss';
+import Head from 'next/head';
 
 export default function Dashboard() {
   return (
-    <div className={styles.container}>
-      <h1>Página Dashboard</h1>
-    </div>
+    <>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+      <div className={styles.container}>
+        <h1>Página Dashboard</h1>
+      </div>
+    </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};

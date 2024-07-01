399b6add129908fe33bafa8fe7022bc4d1802c43
Mxt@DEV <maxta.gamer@gmail.com>
2024-07-01 13:42:07 -0300

    update

--- 01-firstproject-nextjs/src/pages/dashboard/index.tsx
+++ 01-firstproject-nextjs/src/pages/dashboard/index.tsx
@@ -2,17 +2,34 @@ import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 import Head from 'next/head';
+import { Textarea } from '@/src/components/Textarea';
 
 export default function Dashboard() {
   return (
-    <>
+    <div className={styles.conteiner}>
       <Head>
         <title>Meu painel de tarefas</title>
       </Head>
-      <div className={styles.container}>
-        <h1>Página Dashboard</h1>
-      </div>
-    </>
+
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa</h1>
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa publica ?</label>
+              </div>
+
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 
